4cddc5557b4aafda6048db3a8b55e7cf1539e6a8
megh <megh922001@gmail.com>
2022-10-03 22:13:22 -0700

    updated main.cpp for ICE-2

--- OOP3200-F2022-Week2/main.cpp
+++ OOP3200-F2022-Week2/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "Vector2.h"
+#include "LineSegment.h"
 #include <vector>
 
 int main()
@@ -12,6 +13,7 @@ int main()
     for (int i = 0; i < 5; ++i)
     {
         vector2s.push_back(new Vector2(static_cast<float>(i), static_cast<float>(i)));
+
     }
 
     // ranged for loop
@@ -19,6 +21,24 @@ int main()
     {
         std::cout << vector2->to_string() << std::endl;
     }
+
+    // created a four vector to make line segments
+    Vector2 A = Vector2(2, 2);
+    Vector2 B = Vector2(6, 6);
+    Vector2 C = Vector2(10, 10);
+    Vector2 D = Vector2(4, 4);
+
+    // three line segments
+    LineSegment line1 = LineSegment("line1",A,B);
+    LineSegment line2 = LineSegment("line2",D,C);
+    LineSegment line3 = LineSegment("line3",A,C);
+
+    // Displays results
+    std::cout << line1.to_string() << std::endl;
+    std::cout << line2.to_string() << std::endl;
+    std::cout << line3.to_string() << std::endl;
+
+    
 
 }
 
